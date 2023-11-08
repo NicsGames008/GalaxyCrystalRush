4f2b2df5d5f7072afaf7e5a9bbfa7a70e15e813d
Artur Nicolau <92381770+NicsGames008@users.noreply.github.com>
2023-11-08 14:56:18 +0000

    first commit

---
+++ main.lua
@@ -0,0 +1,97 @@
+local world
+local player
+local ground
+local background
+local playerX
+local playerY
+local playerVisible
+local groundVisible
+require "vector2"
+--require "graphicsLoader"
+function love.load()
+    background = love.graphics.newImage("background.jpg")
+    createElements()
+    playerX = 0
+    playerY = 0
+    local canJump = true;
+
+end
+
+function love.update(dt)
+
+    world:update(dt) -- Aktualisiere die Physik-Welt
+
+
+
+    -- Spielersteuerung
+    if love.keyboard.isDown("left") then
+        player.body:applyForce(-player.speed, 0)
+    elseif love.keyboard.isDown("right") then
+        player.body:applyForce(player.speed, 0)
+    end
+
+    -- Springen, wenn der Spieler auf dem Boden ist
+    if love.keyboard.isDown("space") and canJump then
+        player.body:applyForce(0, player.jumpForce)
+        canJump = false -- Prevent jumping until the spacebar is released and pressed again
+    end
+
+    if player.body:isTouching(ground.body) then
+        canJump = true
+    end
+    
+end
+
+function love.draw()
+    drawBackground()
+    drawPlayer(playerX,playerY)
+    drawGround(400,400)
+
+end
+
+
+
+function createElements()
+    love.physics.setMeter(64) 
+    world = love.physics.newWorld(0, 9.81 * 64, true) 
+    player = {}
+    player.body = love.physics.newBody(world, 100, 100, "dynamic")
+    player.shape = love.physics.newCircleShape(20) -- Radius von 20 Pixel
+    player.fixture = love.physics.newFixture(player.body, player.shape)
+    player.body:setFixedRotation(true) -- Verhindere Drehung
+    player.speed = 200 -- Horizontale Geschwindigkeit
+    player.jumpForce = -2000 -- Sprungkraft
+
+    -- Erstelle den Boden (Rechteck)
+    ground = {}
+    ground.body = love.physics.newBody(world, 400, 300, "static")
+    ground.shape = love.physics.newRectangleShape(800, 10) -- Breite von 800 und HÃ¶he von 10 Pixel
+    ground.fixture = love.physics.newFixture(ground.body, ground.shape)
+
+end
+
+
+function drawGround(x,y)
+    love.graphics.setColor(0.008, 0.6, 0.012)
+    love.graphics.rectangle("fill",x,y,1000,150)
+    
+end
+function drawPlayer(x,y)
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", player.body:getX(), player.body:getY(), player.shape:getRadius())
+    
+end
+
+function drawBackground()
+    local width = love.graphics.getWidth()
+    local height = love.graphics.getHeight()
+    love.graphics.polygon("fill", ground.body:getWorldPoints(ground.shape:getPoints()))
+end
+
+function createGround(x, y)
+    ground = {}
+    ground.body = love.physics.newBody(world, x, y, "static")
+    ground.shape = love.physics.newRectangleShape(800, 10)  -- Customize width and height as needed
+    ground.fixture = love.physics.newFixture(ground.body, ground.shape)
+    return ground
+end
---
+++ vector2.lua
@@ -0,0 +1,47 @@
+vector2 = {}
+
+function vector2.new(px,py)
+    return {x = px, y = py}
+end
+
+function vector2.add(vec,inc)
+    local result = vector2.new(0,0)
+    result.x = vec.x + inc.x
+    result.y = vec.y + inc.y
+    return result
+end 
+
+function vector2.sub(vec, dec)
+    local result = vector2.new(0,0)
+    result.x = vec.x - dec.x
+    result.y = vec.y - dec.y
+    return result
+end 
+
+function vector2.mult (vec, n)
+    local result = vector2.new(0, 0)
+    result.x = vec.x * n
+    result.y = vec.y * n
+    return result
+end
+
+function vector2.div (vec, n)
+    local result = vector2.new(0, 0)
+    result.x = vec.x / n
+    result.y = vec.y / n
+    return result
+end
+
+function vector2.magnitude(vec)
+    return math.sqrt(vec.x * vec.x + vec.y * vec.y)
+end
+
+function vector2.normalize(vec)
+    local m = vector2.magnitude(vec)
+    if m ~= 0 then    
+        return vector2.div(vec, m)
+    end
+    return vec
+end
+
+ 
