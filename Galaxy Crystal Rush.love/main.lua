7d7a456b0082dfd237de6879f51b7b096c21dc99
Renner Moritz <mr214@hdm-stuttgart.de>
2023-11-08 17:47:43 +0000

    movement

--- Galaxy Crystal Rush.love/main.lua
+++ Galaxy Crystal Rush.love/main.lua
@@ -9,7 +9,6 @@ local groundVisible
 require "vector2"
 require "graphicsLoader"
 function love.load()
-    background = love.graphics.newImage("Images/background.png")
     createElements()
     playerX = 0
     playerY = 0
@@ -18,17 +17,14 @@ end
 
 function love.update(dt)
 
-    world:update(dt) -- Update the physics world
 
 
-    -- Player controls
     if love.keyboard.isDown("left") then
         player.body:applyForce(-player.speed, 0)
     elseif love.keyboard.isDown("right") then
         player.body:applyForce(player.speed, 0)
     end
 
-    -- Jump when the player is on the ground
     if love.keyboard.isDown("space") and canJump then
         player.body:applyForce(0, player.jumpForce)
         canJump = false -- Prevent jumping until the spacebar is released and pressed again
@@ -53,19 +49,9 @@ function createElements()
     love.physics.setMeter(64) 
     world = love.physics.newWorld(0, 9.81 * 64, true) 
     player = {}
-    player.sprite= love.graphics.newImage("Images/character.png" )
     player.body = love.physics.newBody(world, 100, 100, "dynamic")
-    player.shape = love.physics.newCircleShape(20) -- Radius of 20 pixels
     player.fixture = love.physics.newFixture(player.body, player.shape)
-    player.body:setFixedRotation(true) -- Prevent rotation
-    player.speed = 200 -- Horizontal speed
-    player.jumpForce = -4500 -- Bounce
 
-    -- Create the floor (rectangle)
-    ground = {}
-    ground.body = love.physics.newBody(world, 400, 300, "static")
-    ground.shape = love.physics.newRectangleShape(800, 10) -- Width of 800 and height of 10 pixels
-    ground.fixture = love.physics.newFixture(ground.body, ground.shape)
 
 end
 
@@ -77,7 +63,7 @@ function drawGround(x,y)
 end
 function drawPlayer(x,y)
     love.graphics.setColor(1, 0, 0)
-    love.graphics.draw(player.sprite, player.body:getX(), player.body:getY(), 0, 0.3,0.3)
+    love.graphics.circle("fill", player.body:getX(), player.body:getY(), player.shape:getRadius())
     
 end
 
@@ -85,6 +71,13 @@ function drawBackground()
     local width = love.graphics.getWidth()
     local height = love.graphics.getHeight()
     love.graphics.polygon("fill", ground.body:getWorldPoints(ground.shape:getPoints()))
+end
+
+function drawWall()
+    love.graphics.setColor(0.5, 0.5, 0.5)  -- Set a color for the wall
+    love.graphics.rectangle("fill", (wall.body:getX()-10)/2, (wall.body:getY()-200)/2, 10, 200)
+    love.graphics.print(wall.body:getX(), wall.body:getY(), 0, 0)
+
 end
 
 function createGround(x, y)
