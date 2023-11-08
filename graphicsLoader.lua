4a1acb5203bb26b44bea884f2260bfde6ea6993f
Artur Nicolau <92381770+NicsGames008@users.noreply.github.com>
2023-11-08 15:20:48 +0000

    change of jump, and some other test

--- graphicsLoader.lua
+++ graphicsLoader.lua
@@ -2,5 +2,5 @@
 
 function loadGraphics()
     love.graphics.setBackgroundColor(0.545, 0.824, 0.996)
-    background = love.graphics.newImage("background.png")
+    background = love.graphics.newImage("Images/background.png")
 end
--- main.lua
+++ main.lua
@@ -9,28 +9,26 @@ local groundVisible
 require "vector2"
 require "graphicsLoader"
 function love.load()
-    background = love.graphics.newImage("Images/background.jpg")
+    background = love.graphics.newImage("Images/background.png")
     createElements()
     playerX = 0
     playerY = 0
     local canJump = true;
-
 end
 
 function love.update(dt)
 
-    world:update(dt) -- Aktualisiere die Physik-Welt
+    world:update(dt) -- Update the physics world
 
 
-
-    -- Spielersteuerung
+    -- Player controls
     if love.keyboard.isDown("left") then
         player.body:applyForce(-player.speed, 0)
     elseif love.keyboard.isDown("right") then
         player.body:applyForce(player.speed, 0)
     end
 
-    -- Springen, wenn der Spieler auf dem Boden ist
+    -- Jump when the player is on the ground
     if love.keyboard.isDown("space") and canJump then
         player.body:applyForce(0, player.jumpForce)
         canJump = false -- Prevent jumping until the spacebar is released and pressed again
@@ -55,17 +53,18 @@ function createElements()
     love.physics.setMeter(64) 
     world = love.physics.newWorld(0, 9.81 * 64, true) 
     player = {}
+    player.shape
     player.body = love.physics.newBody(world, 100, 100, "dynamic")
-    player.shape = love.physics.newCircleShape(20) -- Radius von 20 Pixel
+    player.shape = love.physics.newCircleShape(20) -- Radius of 20 pixels
     player.fixture = love.physics.newFixture(player.body, player.shape)
-    player.body:setFixedRotation(true) -- Verhindere Drehung
-    player.speed = 200 -- Horizontale Geschwindigkeit
-    player.jumpForce = -2000 -- Sprungkraft
+    player.body:setFixedRotation(true) -- Prevent rotation
+    player.speed = 200 -- Horizontal speed
+    player.jumpForce = -4500 -- Bounce
 
-    -- Erstelle den Boden (Rechteck)
+    -- Create the floor (rectangle)
     ground = {}
     ground.body = love.physics.newBody(world, 400, 300, "static")
-    ground.shape = love.physics.newRectangleShape(800, 10) -- Breite von 800 und Höhe von 10 Pixel
+    ground.shape = love.physics.newRectangleShape(800, 10) -- Width of 800 and height of 10 pixels
     ground.fixture = love.physics.newFixture(ground.body, ground.shape)
 
 end
